ba05b9d687c836c4a7d8a2d335fcc7529c46387d
karthikgarikina <garikinakarthik459@gmail.com>
2025-12-06 11:35:07 +0530

    learnt get,post,put

--- Back-End/Express/index.js
+++ Back-End/Express/index.js
@@ -1,11 +1,21 @@
 import express from 'express'
 
 const app=express();
+app.use(express.json());
 app.get('/users',(req,res)=>{
-    res.send("Hello this is from backend");
+    res.send("Users page");
 });
-app.get('/get-users',(req,res)=>{
-    res.send("Hello this is from backend for get-users");
+app.post('/get-user',(req,res)=>{
+    let data=req.body.data;
+    let result=parseInt(data);
+    result=10*result;
+    
+    res.send(`Given data is ${data}, result for it: ${result}`);
+});
+app.put('/add-user',(req,res)=>{
+    let data=req.body;
+    console.log(data);
+    res.end("data added");
 });
 app.listen(7007,()=>{
     console.log("Server rinning on port 7007");
